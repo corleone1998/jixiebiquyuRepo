970b8e5dc58c305974402456f82c9f232a10e000
wangyi_linux <3078735056@qq.com>
2023-12-19 17:05:09 +0800

    产生了随机污渍区域（10个）与k-means算法分割图像模块，但未实现区域分割

---
+++ fenge/src/K-means.cpp
@@ -0,0 +1,50 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+    // 加载图像
+    Mat img = imread("../../tuxiang/build/Stained_area.png");
+    if (img.empty()) {
+        cout << "Could not read the image" << endl;
+        return 1;
+    }
+
+    // 将图像转换为二维样本数据
+    Mat samples(img.rows * img.cols, 3, CV_32F);
+    for (int y = 0; y < img.rows; y++) {
+        for (int x = 0; x < img.cols; x++) {
+            for (int z = 0; z < 3; z++) {
+                samples.at<float>(y + x * img.rows, z) = img.at<Vec3b>(y, x)[z];
+            }
+        }
+    }
+
+    // 应用k-means算法
+    int clusterCount = 8;
+    Mat labels;
+    int attempts = 5;
+    Mat centers;
+    kmeans(samples, clusterCount, labels, TermCriteria(TermCriteria::EPS+TermCriteria::COUNT, 10, 1.0), attempts, KMEANS_PP_CENTERS, centers);
+
+    // 用聚类中心的颜色替换图像中的每个像素
+    Mat new_image(img.size(), img.type());
+    for (int y = 0; y < img.rows; y++) {
+        for (int x = 0; x < img.cols; x++) {
+            int cluster_idx = labels.at<int>(y + x * img.rows, 0);
+            new_image.at<Vec3b>(y, x)[0] = centers.at<float>(cluster_idx, 0);
+            new_image.at<Vec3b>(y, x)[1] = centers.at<float>(cluster_idx, 1);
+            new_image.at<Vec3b>(y, x)[2] = centers.at<float>(cluster_idx, 2);
+        }
+    }
+
+    // 显示原始图像和聚类后的图像
+    imshow("original image", img);
+    imshow("clustered image", new_image);
+
+    waitKey(0);
+
+    return 0;
+}
---
+++ tuxiang/src/Map.cpp
@@ -0,0 +1,55 @@
+#include <opencv2/opencv.hpp>
+#include <random>
+
+
+// #include <mutex>
+
+
+int main() {
+    // 图像的宽度和高度
+    const int width = 1920;
+    const int height = 1080;
+
+    // 创建一个白色背景的图像
+    cv::Mat image = cv::Mat::zeros(height, width, CV_8UC3);
+    image.setTo(cv::Scalar(255, 255, 255));
+
+    // 设置方块个数
+    int area_number = 10;
+       // 每个方块的尺寸
+    int block_size = 50;
+
+    // 循环产生每个随机方块
+for (size_t i = 0; i < area_number; i++)
+{
+    
+    // 创建随机种子及随机序列
+    std::random_device rd;
+    std::mt19937 mt(rd());
+
+    // 确保随机位置对齐到1920*1080像素的网格上
+    std::uniform_int_distribution<int> dist_x(0, width-1); // 0到1919
+    std::uniform_int_distribution<int> dist_y(0, height-1); // 0到1079
+
+    // 生成一个随机位置
+    int x_start = dist_x(mt);
+    int y_start = dist_y(mt) ;
+
+    // 确保绿色区域不小于50x50
+    cv::Rect greenRect(x_start, y_start, 50, 50);
+
+    // 在图像中绘制绿色区域
+    cv::rectangle(image, greenRect, cv::Scalar(0, 255, 0), cv::FILLED);
+}
+    // 用红色线将图像分为左右两个相等的部分
+    // cv::line(image, cv::Point(width / 2, 0), cv::Point(width / 2, height), cv::Scalar(0, 0, 255), 3);
+
+    // 显示图像
+    cv::imshow("Image with Random Stained Area", image);
+    cv::waitKey(0);
+
+    // 保存清洗区域“Stained_area”图像
+    cv::imwrite("Stained_area.png", image);
+
+    return 0;
+}
