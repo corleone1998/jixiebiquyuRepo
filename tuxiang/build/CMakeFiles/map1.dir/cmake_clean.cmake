f875be17dff13832f778b120db19c7cca179a788
wangyi_linux <3078735056@qq.com>
2023-12-19 12:02:26 +0800

    正式开始实现机械臂作业区域的随机生成以及面积等分

---
+++ tuxiang/build/CMakeFiles/map1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/map1"
+  "../bin/map1.pdb"
+  "CMakeFiles/map1.dir/map1.cpp.o"
+  "CMakeFiles/map1.dir/map1.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/map1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
